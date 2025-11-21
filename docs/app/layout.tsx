05292577669b6e5d71b2aeda1a751f116ca833bb
mattc0llier <matt.collier@hotmail.com>
2025-11-21 11:16:35 +0000

    add speed insights

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/global.css';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { HomeLayout } from '@/components/layout/home';
@@ -79,6 +80,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         </RootProvider>
         <Toaster />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
